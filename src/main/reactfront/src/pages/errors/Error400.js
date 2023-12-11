9ee10494cfe260bfeee41e7f86dbf96ceaf0aa74
wngudtlr10 <wngudtlr10@naver.com>
2023-12-11 15:47:13 +0900

    error pages

---
+++ src/main/reactfront/src/pages/errors/Error400.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Main() {
+    return (
+        <div className="main-container">
+            <h1 className="main-title">Welcome to the Main Page</h1>
+            <div className="main-desc">
+                <p>This is your new cool Main page.</p>
+                <p>You can add more contents here.</p>
+                <p>Error400</p>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
