254da173dd4dd603436657313a7544a4d389adb4
accelKim <dnjsgh567877@gmail.com>
2023-12-27 02:12:50 +0900

    12.27

--- src/main/reactfront/src/pages/admin/views/gathering/Gathering.js
+++ src/main/reactfront/src/pages/admin/views/gathering/Gathering.js
@@ -40,6 +40,10 @@ const Gathering = () => {
         _style: { width: '20%' }
       },
       {
+        key : 'status',
+        _style: { width: '20%' }
+      },
+      {
         key: 'show_details',
         label: '',
         _style: { width: '1%' },
@@ -90,21 +94,32 @@ const Gathering = () => {
       deleteGathering(gatheringId);
     }
   };
-
-    const getBadge = (status) => {
-      switch (status) {
-        case 'Active':
-          return 'success'
-        case 'Inactive':
-          return 'secondary'
-        case 'Pending':
-          return 'warning'
-        case 'Banned':
-          return 'danger'
-        default:
-          return 'primary'
-      }
+  const getBadge = (status) => {
+    switch (status) {
+      case "OPEN":
+        return 'success'; // 대기중 - 회색
+      case "CLOSED":
+        return 'secondary';   // 수락완료 - 초록색
+      case "FINISHED":
+        return 'danger';    // 반려 - 빨간색
+      default:
+        return 'primary';    // 기본값 설정 (필요에 따라 변경 가능)
     }
+  };
+
+
+  const getBadgeText = (status) => {
+    switch (status) {
+      case "OPEN":
+        return "참여가능";
+      case "CLOSED":
+        return "참여마감";
+      case "FINISHED":
+        return "종료";
+      default:
+        return '알 수 없음';
+    }
+  };
     const toggleDetails = (index) => {
       const position = details.indexOf(index)
       let newDetails = details.slice()
@@ -151,6 +166,11 @@ const Gathering = () => {
               <CAvatar src={item.image}></CAvatar>
             </td>
           ),
+          status: (item) => (
+            <td>
+              <CBadge color={getBadge(item.status)}>{getBadgeText(item.status)}</CBadge>
+            </td>
+        ),
           show_details: (item) => {
             return (
               <td className="py-2">
@@ -176,9 +196,10 @@ const Gathering = () => {
                   <h4>{item.title}</h4>
                   <p className="text-muted">내용 : {item.intro}</p>
                   <p className="text-muted">주소 : {item.fullAddress + item.subAddress}</p>
+                  <p className="text-muted">우편번호 : {item.zoneCode}</p>
                   <p className="text-muted">마감일 : {item.deadline}</p>
                   <p className="text-muted">시작일 : {item.startAt}</p>
-                   <GatheringModify gatheringId={item.gatheringId} onUpdate={handleUpdateGathering}/>
+                   {/*<GatheringModify gatheringId={item.gatheringId} onUpdate={handleUpdateGathering}/>*/}
                   <CButton size="sm" color="danger" className="ml-1" style={{color:'white'}}
                   onClick={() => handleDeleteClick(item.gatheringId)}
                   >
