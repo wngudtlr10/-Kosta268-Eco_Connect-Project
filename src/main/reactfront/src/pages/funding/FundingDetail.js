56b28e9517307f50ad36953a1e8cec3bedad1e21
accelKim <dnjsgh567877@gmail.com>
2023-12-27 17:30:28 +0900

    12.27

--- src/main/reactfront/src/pages/funding/FundingDetail.js
+++ src/main/reactfront/src/pages/funding/FundingDetail.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import moment from 'moment';
 import Layout from "../../components/Layout/Layout";
 import "./FundingView.css";
+import './FundingDetail.css'
+import FundingDetailIntro from "./FundingDetailIntro";
 
 
 const FundingDetail = () => {
@@ -48,29 +50,36 @@ const FundingDetail = () => {
             <div className="funding-detail-image">
                 <img src="../"></img> {/* 수정: <img> 태그로 바꿨습니다 */}
             </div>
-            <div>
-                <h1 style={{textAlign: "center"}}>Funding Detail</h1>
-                <ul>
-                    {fundings.map(funding => (
-                        <li key={funding.id} style={listItemStyle}>
-                            <div>1. 제목: {funding.title}</div>
-                            <div>2. 저자: {funding.author}</div>
-                            <div>3. 카테고리 ID: {funding.fundingCategoryId} </div>
-                            <div>4. 상태: {funding.status}</div>
-                            <div>5. 내용: {funding.content}</div>
-                            <div>6. 생성 날짜: {formatDate(funding.createdAt).toLocaleString()}</div>
-                            <div>7. 종료 날짜: {formatDate(funding.endAt).toLocaleString()}</div>
-                            <div>8. 좋아요: {funding.likes}</div>
-                            <div>9. 수정 날짜: {formatDate(funding.modify_at).toLocaleString()}</div>
-                            <div>10. 시작 날짜: {formatDate(funding.startAt).toLocaleString()}</div>
-                            <br/>
-                            <br/>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </Layout>
-    );
+            {/* <div className="funding-intro-box">
+                <div className="funding-intro-title">프로젝트 스토리</div>
+                <div className="funding-intro-image-wrap"><img className="funding-intro-image" src="https://i.postimg.cc/5Nnzzjvy/Funding-intro.png"></img></div>
+             </div> */}
+              <div className="intro-warp">
+              <FundingDetailIntro style={{}}/>
+              </div>
+                            <div>
+                                <h1 style={{ textAlign: "center" }}>Funding Detail</h1>
+                                <ul>
+                                    {fundings.map(funding => (
+                                        <li key={funding.id} style={listItemStyle}>
+                                            <div>1. 제목: {funding.title}</div>
+                                            <div>2. 저자: {funding.author}</div>
+                                            <div>3. 카테고리 ID: {funding.fundingCategoryId} </div>
+                                            <div>4. 상태: {funding.status}</div>
+                                            <div>5. 내용: {funding.content}</div>
+                                            <div>6. 생성 날짜: {formatDate(funding.createdAt).toLocaleString()}</div>
+                                            <div>7. 종료 날짜: {formatDate(funding.endAt).toLocaleString()}</div>
+                                            <div>8. 좋아요: {funding.likes}</div>
+                                            <div>9. 수정 날짜: {formatDate(funding.modify_at).toLocaleString()}</div>
+                                            <div>10. 시작 날짜: {formatDate(funding.startAt).toLocaleString()}</div>
+                                            <br />
+                                            <br />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </Layout>
+                        );
 }
 
-export default FundingDetail
+                        export default FundingDetail
